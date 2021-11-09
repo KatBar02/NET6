89960119dbadbddd1598ebced0d6cc36a128ee96
Katarína Bartošová <bartosova.katarin@gmail.com>
2021-11-09 14:47:24 +0100

    PersonData - zapisovanie Person do súboru

---
+++ HelloWorldCore/HelloWorldCore/Data/PersonData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorldCore.Model;
+
+namespace HelloWorldCore.Data
+{
+    class PersonData
+    {
+        //static string PersonDataFile { get; set; } // statická položka, globální proměnná
+        const string personDataFile = "people.txt"; //druhá možnosť
+
+        /// <summary>
+        /// metoda pro uložení/připojení jedne osoby do souboru
+        /// </summary>
+        public static void SavePerson(Person person)
+        {
+            var line = $"{person.FirstName};{person.LastName};{person.Birthdate};{person.HomeAddress.City};{person.HomeAddress.Street}";
+            line += Environment.NewLine;
+            File.AppendAllText(personDataFile, line);
+        }
+    }
+}
--- HelloWorldCore/HelloWorldCore/PersonMetody.cs
+++ HelloWorldCore/HelloWorldCore/PersonMetody.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorldCore.Data;
 using HelloWorldCore.Model;
 
 namespace HelloWorldCore
@@ -19,10 +20,12 @@ namespace HelloWorldCore
 
             //Car c = new Car("1a1 3636", "blue", 32000);
 
-            Person p5 = new Person("Martin", "Malík", new DateTime(2010, 12, 02));
+            Person p5 = new Person("Sára", "Kolumbusová", new DateTime(1810, 01, 15));
             //p5.HomeAddress = new Address("Praha","česká"); // musí byť inicializovaná/vytvorená Address pred priradením hodnoty, môže byť už v Person v konstruktore
-            p5.HomeAddress.City = "Praha";
-            Console.WriteLine(p5);
+            p5.HomeAddress.City = "Londýn";
+            p5.HomeAddress.Street = "Londýnská";
+            //Console.WriteLine(p5);
+            PersonData.SavePerson(p5);
         }
 
         private static void StarsiOsoba()
