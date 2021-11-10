48f2dd73867fe9b9c394ea363e904aec52f7cdb9
Katarína Bartošová <bartosova.katarin@gmail.com>
2021-11-10 11:41:19 +0100

    výpočet dňa z čísla

---
+++ HelloWorldCore/HelloWorldCore/MethodsDay3.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCore
+{
+    class MethodsDay3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayNameCZ(2));
+            Console.WriteLine(DayNameArrayCZ(2));
+            Console.WriteLine(DayNameListCZ(2));
+            Console.WriteLine(IsWorkDay(2));
+
+
+        }
+
+        public static string DayNameCZ(int dayNmbr)
+        {
+         
+            switch (dayNmbr)
+            {
+                case 1: 
+                    return "pondelok";
+                case 2: 
+                    return "utorok";
+                default: 
+                    return "číslo nereprezentuje deň v týždni";
+            }
+        }
+
+        public static bool IsWorkDay(int dayNmbr)
+        {
+
+            switch (dayNmbr)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return true;
+                case 6:
+                    return false;
+                default:
+                    throw new Exception("číslo dňa musí byť 1-7");
+            }
+        }
+
+        public static string DayNameArrayCZ(int dayNmbr)
+        {
+
+            string[] days = new[] { "pondelok", "utorok", "streda"};
+            return days[dayNmbr-1];
+        }
+
+        public static string DayNameListCZ(int dayNmbr)
+        {
+
+            List<string> days = new List<string> { "pondelok", "utorok", "streda" };
+            return days[dayNmbr - 1];
+        }
+    }
+}
--- HelloWorldCore/HelloWorldCore/PointMath.cs
+++ HelloWorldCore/HelloWorldCore/PointMath.cs
@@ -9,14 +9,14 @@ namespace HelloWorldCore
 {
     class PointMath
     {
-        static void Main(string[] args)
-        {
-            Point2D point = new Point2D(5, 10);
-            Point2D point2 = new Point2D(2, 4);
-            //Console.WriteLine(point.ToString());
-            //Console.WriteLine(SumPoint(point, point2));
-            Console.WriteLine($"Vysledek sčítání pro body p1 = [{point.X},{point.Y}] a p2 = [{point2.X},{point2.Y}] je [{SumPoint(point, point2).X},{SumPoint(point, point2).Y}]");
-        }
+        //static void Main(string[] args)
+        //{
+        //    Point2D point = new Point2D(5, 10);
+        //    Point2D point2 = new Point2D(2, 4);
+        //    //Console.WriteLine(point.ToString());
+        //    //Console.WriteLine(SumPoint(point, point2));
+        //    Console.WriteLine($"Vysledek sčítání pro body p1 = [{point.X},{point.Y}] a p2 = [{point2.X},{point2.Y}] je [{SumPoint(point, point2).X},{SumPoint(point, point2).Y}]");
+        //}
 
         private static Point2D SumPoint(Point2D p1, Point2D p2)
         {
