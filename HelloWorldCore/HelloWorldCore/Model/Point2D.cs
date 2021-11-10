c05c5128f91d4a810b58a1f28e829edf8a899a34
Katarína Bartošová <bartosova.katarin@gmail.com>
2021-11-10 11:02:24 +0100

    sčítanie bodov v instanci (na rozdiel od predošlého statického)

--- HelloWorldCore/HelloWorldCore/Model/Point2D.cs
+++ HelloWorldCore/HelloWorldCore/Model/Point2D.cs
@@ -27,5 +27,24 @@ namespace HelloWorldCore.Model
         {
             return $"Vysledek pro X = {X} a Y = {Y} je {Plocha()}";
         }
+        /// <summary>
+        /// Přičte bod zo vstupního parametru k bodu. Výsledkem je nový bod.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns>Nový bod</returns>
+        public Point2D AddAnotherPoint (Point2D point)
+        {
+            Point2D soucetBodu = new Point2D(X + point.X, Y + point.Y);
+            return soucetBodu;
+        }
+        /// <summary>
+        /// Modifikuje tento bod, přičte hodnotu vstupního parametru point k sobě samotnému.
+        /// </summary>
+        /// <param name="point"></param>
+        public void AddPoint(Point2D point)
+        {
+            X += point.X;
+            Y += point.Y;
+        }
     }
 }
