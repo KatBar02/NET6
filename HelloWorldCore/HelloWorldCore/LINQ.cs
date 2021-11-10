485c2be6eef0f2622432d1e1cfe8c585ea3b6b92
Katarína Bartošová <bartosova.katarin@gmail.com>
2021-11-10 14:17:54 +0100

    LINQ - operátory

--- HelloWorldCore/HelloWorldCore/LINQ.cs
+++ HelloWorldCore/HelloWorldCore/LINQ.cs
@@ -12,15 +12,38 @@ namespace HelloWorldCore
         static void Main(string[] args)
         {
             int[] numbers = { 1, 2, -30, 4, 20, 256, -45, 99 };
-
-            var result = numbers.Where(number => number >= 0); //number - pomenovanie jednotlivých itemov, "number >=0" podmienak na vyhodnotenie TRUE/FALSE
-
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
         }
 
+        private static void LINQoperator(int[] numbers)
+        {
+            // number - pomenovanie jednotlivých itemov, "number >=0" podmienak na vyhodnotenie TRUE/ FALSE
+            //var result = numbers.Where(number => number >= 0 && number < 100); 
+            /*
+            var result = numbers
+                .Where(number => number >= 0)
+                .Where(number => number < 100); 
+            */
 
+            // razeni, - = desc
+            //var result = numbers.OrderBy(n => -n); 
+
+            //take (top 5) a skip (preskoč prvých 5), TakeWhile (vracaj pokiaľ platí podmienka), SkipWhile (preskakuj pokilaľ platí podmienka
+            //var result = numbers.SkipWhile(n => n > 0);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // Sum, Max, Average
+            //var sum = numbers.Sum();
+
+            // first, last
+            var result = numbers.OrderBy(n => n).First();
+
+
+
+            Console.WriteLine(result);
+        }
     }
 }
